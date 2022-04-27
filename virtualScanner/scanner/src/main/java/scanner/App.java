3481dd336741ee3f5cb35cf327ee8e5ae1359562
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2022-04-28 00:08:20 +0530

    Add files via upload

---
+++ virtualScanner/scanner/src/main/java/scanner/App.java
@@ -0,0 +1,20 @@
+package scanner;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
+import java.util.*;
+import javax.management.RuntimeErrorException;
+import java.util.HashMap;
+import com.google.gson.*;
+
+public class App 
+{
+    
+}
